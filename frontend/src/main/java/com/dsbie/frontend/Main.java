61022df1250cbff61833f879b2f06920f44a7dcd
rabbitharelu@gmail.com <rabbitharelu@gmail.com>
2024-03-28 21:25:04 +0800

    frontend

--- frontend/src/main/java/com/dsbie/frontend/Main.java
+++ frontend/src/main/java/com/dsbie/frontend/Main.java
@@ -3,6 +3,7 @@ package com.dsbie.frontend;
 import com.dsbie.frontend.frame.DsbieJFrame;
 import com.dsbie.frontend.utils.FontUtil;
 import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLaf;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,6 +17,7 @@ public class Main {
             UIManager.put("Button.arc", 50);  // 设置按钮的弧度
             UIManager.put("Component.focusWidth", 0);  // 设置组件的焦点边框宽度
             UIManager.put("TextComponent.arc", 10);
+            UIManager.put("OptionPane.showIcon", true);
             FontUtil.initUIFont();
             dsbieJFrame = new DsbieJFrame();
         });
