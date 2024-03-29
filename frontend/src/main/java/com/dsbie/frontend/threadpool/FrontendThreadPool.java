a9f28625d5e91fdfbe8be6710cc2614232fda492
rabbitharelu@gmail.com <rabbitharelu@gmail.com>
2024-03-29 14:13:26 +0800

    frontend-前端启动虚拟线程池

---
+++ frontend/src/main/java/com/dsbie/frontend/threadpool/FrontendThreadPool.java
@@ -0,0 +1,30 @@
+package com.dsbie.frontend.threadpool;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author lsl
+ * @version 1.0
+ * @date 2024年03月29日 13:51
+ */
+@Slf4j
+@Data
+public class FrontendThreadPool {
+
+    private static final FrontendThreadPool INSTANCE = new FrontendThreadPool();
+
+    private ExecutorService executorService;
+
+    private FrontendThreadPool() {
+        log.info("初始化虚拟线程池");
+        executorService = Executors.newVirtualThreadPerTaskExecutor();
+    }
+
+    public static FrontendThreadPool getInstance() {
+        return INSTANCE;
+    }
+}
